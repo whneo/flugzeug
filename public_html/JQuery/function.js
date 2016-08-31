e4ee25fdfc2d63e122c4eefccf29af2021f11bb9
sunny <sunny@DESKTOP-839KGT4.mshome.net>
2016-08-31 23:33:36 +0200

    fertig

--- public_html/JQuery/function.js
+++ public_html/JQuery/function.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
                     $('#divId').animate({left: '150px', top: '400px'}, 800, function () {
                         $('#divId').animate({left: '300px', top: '200px'}, 800, function () {
                             $('#divId').animate({left: '600px', top: '200px'}, 800, function () {
-                                $('#divId').rotate({angle: 0, animateTo: 0}, 800);
+                                $('#divId').rotate({angle: 12, animateTo: 0}, 800);
                                 $('#divId').animate({left: '900px', top: '400px'}, 800, function () {
                                     $('#divId').animate({left: '1000px', top: '400px'}, 800);
                                 });
